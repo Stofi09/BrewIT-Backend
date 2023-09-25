7f87eec4cddc8e9ec49700b1b9ba2feabf0e82db
Stofi09 <kristof.solak@gmail.com>
2023-09-25 10:19:30 +0200

    Basic user entity created

--- src/main/java/com/brewit/backend/model/BrewITUser.java
+++ src/main/java/com/brewit/backend/model/BrewITUser.java
@@ -1,7 +1,57 @@
 package com.brewit.backend.model;
 
-import jakarta.persistence.Entity;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.persistence.*;
 
 @Entity
+@Table(name = "brewit_user")
 public class BrewITUser {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false)
+    private Long id;
+
+    @Column(name = "name", nullable = false, unique = true)
+    private String name;
+
+    @JsonIgnore
+    @Column(name = "password", nullable = false, length = 1000)
+    private String password;
+
+    @Column(name = "email", nullable = false, unique = true, length = 350)
+    private String email;
+
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
 }
