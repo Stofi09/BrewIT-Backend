04d3d6e025555288209fa74f20aed7c6209a960a
Stofi09 <kristof.solak@gmail.com>
2023-09-25 10:32:03 +0200

    Beer entity created

---
+++ src/main/java/com/brewit/backend/model/BeerItem.java
@@ -0,0 +1,54 @@
+package com.brewit.backend.model;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "brewit_beer")
+public class BeerItem {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false)
+    private Long id;
+
+    @Column(name = "name", nullable = false, unique = true)
+    private String name;
+
+    @Column(name = "description", nullable = false, columnDefinition = "TEXT")
+    private String description;
+
+    @Column(name = "unit_price")
+    private Double abv;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Double getAbv() {
+        return abv;
+    }
+
+    public void setAbv(Double abv) {
+        this.abv = abv;
+    }
+}
